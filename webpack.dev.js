55bf6876588d814b89a023e49053969a222f0ce7
solid-000 <razbc12@gmail.com>
2026-01-08 17:41:21 +1100

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/template.html"],
+    //open chrome on mac//
+    open: {
+      app: {
+        name: path.join(process.env.HOME, "Applications/Google Chrome.app"),
+      },
+    },
+    hot: true,
+    // open: true, // Open default browser
+  },
+});
